5d666f91c925d238399db0c35f924a4fe3f6a99a
styven <styvenn16@gmail.com>
2024-11-19 10:53:35 -0300

    classes

---
+++ Classes/main.js
@@ -0,0 +1,107 @@
+//Classes
+/*
+class Person {
+    constructor(name){
+        console.log("Ola ", name);
+    }
+}
+
+const person = new Person("João");
+
+class Product {
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+}
+
+const product = new Product("Camisa", 20);
+console.log(product.name);
+console.log(product.price);
+const product1 = new Product("teclado", 22);
+console.log(product1.name);
+console.log(product1.price);
+
+
+class User{
+    constructor(name, email){
+        this.name = name;
+        this.email = email;
+    }
+    sendEmail(){
+        console.log(`O ${this.name} foi enviado para ${this.email}`);
+
+    }
+}
+
+const user = new User("João", "joao@gmail.com");
+user.sendEmail();
+
+class User{
+   static showMessage(){
+        console.log("Ola");
+    }
+}
+User.showMessage();
+
+class Animal{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    eat(){
+        console.log("O animal está comendo");
+    }
+}
+
+class Dog extends Animal{
+    constructor(name, age, breed){
+        super(name, age);
+        this.breed = breed;
+    }
+    bark(){
+        console.log("O cachorro está latindo");
+    }
+}
+const dog = new Dog("Rex", 3, "Labrador");
+console.log(dog.name);
+
+
+
+
+class Animal{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    eat(){
+        console.log("O animal está comendo");
+    }
+}
+
+class Dog extends Animal{
+    constructor(name, age, breed){
+        super(name, age);
+        this.breed = breed;
+    }
+    bark(){
+        console.log("O cachorro está latindo");
+    }
+    eat(){
+        console.log("O cachorro está comendo");
+    }
+}
+const dog = new Dog("Rex", 3, "Labrador");
+console.log(dog);
+dog.eat();
+
+
+const address = {
+    street: "Rua dos bobos",
+    number: 0,
+    city: "São Paulo",
+    state: "SP",
+    country: "Brasil"
+}
+console.log(address.__proto__);
+*/
